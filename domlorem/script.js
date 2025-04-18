ca9106cffe654b4c81633e8ede708f101e9adfe9
mubiloshka <matkarimova110@icloud.com>
2025-04-19 00:02:37 +0500

    Add files via upload

---
+++ domlorem/script.js
@@ -0,0 +1,6 @@
+const background = prompt("Введите цвет заднего фона");
+document.body.style.backgroundColor = background ;
+
+const tsvet = prompt("Введите цвет букв ");
+const text = document.getElementById("text");
+text.style.color = tsvet ;
